717dc2747c7f17c8a8e3f99c251dd1a25a409134
AkhshyBalakannan <akhshy.balakannan@aspiresys.com>
2022-12-03 17:12:23 +0530

    Testing for auto deploy

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def hello_world():
     return "Hello Akhshy!! Its working"
 
+@app.route("/test")
+def hello_world():
+    return "Hello Akhshy!! Its working even after updating in github"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
